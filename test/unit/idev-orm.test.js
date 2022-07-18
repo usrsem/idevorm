fc526670e367af2d82d6902f6e698788c2bbfffb
idevtier <idevtier@gmail.com>
2022-07-18 14:21:29 +0200

    Adding tests for IdevOrm

---
+++ test/unit/idev-orm.test.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const assert = require('assert');
+const { describe, it } = require('mocha');
+const IdevOrm = require('../../lib/idev-orm.js');
+
+module.exports = () => describe('IdevOrm', () => {
+  describe('getReposiotry', () => {
+    it('Returns valid repository working with db\'s pool', () => {
+    });
+
+    it('Fails on getting repository for not exisiting schema', () => {
+    });
+  });
+});
